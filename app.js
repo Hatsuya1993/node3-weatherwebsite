f99e3922f2905fe53e7f14e68848d0dc464ab4f0
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-06-30 11:43:21 +0800

    Set up app for heroku

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 const path = require("path");
 const viewsPath = path.join(__dirname, "../webserver/templates/views");
 const partialsPath = path.join(__dirname, "../webserver/templates/partials");
-const forecast = require("./source/forecast/forecast")
-const geocode = require("./source/utils/geocode")
-const request = require("request")
+const forecast = require("./source/forecast/forecast");
+const geocode = require("./source/utils/geocode");
+const request = require("request");
 const hbs = require("hbs");
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
@@ -40,55 +41,48 @@ app.get("/test", (req, res) => {
 app.get("/weather", (req, res) => {
   if (!req.query.location) {
     return res.send({
-      error: "Provide a search term for weather"
-    })
+      error: "Provide a search term for weather",
+    });
   }
 
   if (req.query.location == undefined) {
-    console.log("Please provide a country")
+    console.log("Please provide a country");
   } else {
-    geocode(req.query.location, (error, {
-      lat,
-      lon,
-      loca
-    } = {}) => {
+    geocode(req.query.location, (error, { lat, lon, loca } = {}) => {
       if (error == "Error") {
         res.send({
-          error: "API error"
+          error: "API error",
         });
       } else if (error == "City") {
         res.send({
-          error: "Country error"
+          error: "Country error",
         });
       } else if (error == "search") {
         res.send({
-          error: "Send a valid location"
-        })
+          error: "Send a valid location",
+        });
       } else {
         console.log(lat, lon, loca);
       }
-      forecast(lat, lon, (error, {
-        cloud,
-        type
-      } = {}) => {
+      forecast(lat, lon, (error, { cloud, type } = {}) => {
         if (error == "Country") {
           res.send({
-            error: "Country invalid"
-          })
+            error: "Country invalid",
+          });
         } else if (error == "API") {
           res.send({
-            error: "API Invalid"
-          })
+            error: "API Invalid",
+          });
         } else {
           res.render("weather", {
             title: "Weather",
             location: loca,
             cloud: cloud,
             type: type,
-            name: "Hazrul"
+            name: "Hazrul",
           });
         }
-      })
+      });
     });
   }
 });
@@ -96,14 +90,14 @@ app.get("/weather", (req, res) => {
 app.get("/products", (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: "Provide a search term"
-    })
+      error: "Provide a search term",
+    });
   }
-  console.log(req.query.search)
+  console.log(req.query.search);
   return res.send({
-    product: []
-  })
-})
+    product: [],
+  });
+});
 
 app.get("/help/*", (req, res) => {
   res.render("errorHelp", {
@@ -119,6 +113,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Running server 3000");
+app.listen(port, () => {
+  console.log("Running server " + port);
 });
